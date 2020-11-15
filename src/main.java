4da2231c612b75ce1143d470295e2f467c97e8de
altyshaibraim <74196750+altyshaibraim@users.noreply.github.com>
2020-11-15 01:01:16 -0600

    updated the structure

--- src/main.java
+++ src/main.java
@@ -1,26 +1,32 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("             What happens when Git configures with IntelliJ:");
-        System.out.println("An IntelliJ Project becomes Git Repo.");
-        System.out.println("");
-        System.out.println("             How to turn IntelliJ Project into Git Repo:");
-        System.out.println("1. Create Project. \n 2. Create new Class(File) under src package.");
-        System.out.println("3. On the menu bar choose VCS. \n 4. Enable Version Control Integration (if it's not there," +
-                " then it is already installed).");
-        System.out.println("!!! If Git is succesfully enabled, then on the left bottom a green pop-up msg will appear.");
-        System.out.println("");
-        System.out.println("5. After you modified the file, to commit there a green check button on the right top corner" +
-                " OR VSC on the menu bar -> Commit");
-        System.out.println("!!! To see the history of commits, click on Version Control on the left bottom -> Log" );
-        System.out.println("!!! To edit a commit, right click on selected commit -> Edit Commit Msg");
-        System.out.println("");
-        System.out.println("              To link GitHub account to IntelliJ:");
-        System.out.println("File -> Settings -> GitHub -> Add Account -> Login");
-        System.out.println("");
-        System.out.println("              To share Project on GitHub Website:");
-        System.out.println("VCS -> import into Version Control -> Share Project on GitHub -> naming -> Share");
-        System.out.println("");
-        System.out.println("              To push local commit to remote Repo:");
-        System.out.println("VCS -> Git -> Push(green upright arrow) -> Select commit -> Push");
+        /*                            What happens when Git configures with IntelliJ:
+            An IntelliJ Project becomes Git Repo.
+        
+                                      To turn IntelliJ Project into Git Repo:
+            File -> New Project -> New Class(File) under src package -> on the menu bar select 'VCS' -> Enable Version Control Integration 
+                                                                                          (if it's not there, then it is already installed)
+            !!! If Git is succesfully enabled, then on the left bottom a green pop-up msg will appear.
+        
+                                      To commit on changes in the file:
+            After you modified the file, to commit there is a 'green check button' on the right top corner OR 'VSC' on the menu bar -> Commit
+                               
+                                      To see the history of commits:
+            Click on 'Version Control' on the left bottom -> Log
+            
+                                      To edit a commit:
+            Right click on selected commit -> Edit Commit Message
+        
+                                      To link GitHub account to IntelliJ:
+            File -> Settings -> GitHub -> Add Account -> Login
+       
+                                      To share Project on GitHub Website:
+            VCS -> Import into Version Control -> Share Project on GitHub -> naming -> Share"
+    
+                                      To push local commit to remote Repo:
+            VCS -> Git -> Push(green upright arrow) -> select commit -> Push
+            
+                                      To open remote repo directly from IntelliJ:
+            Right click on the project -> Open on GitHub 
     }
 }
